5322333eb8b509aaaa8ecbc704ac271a37a1c2e2
Eden Mesfun <emesfun@gmail.com>
2018-03-20 22:41:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    resp.write "My name is "
+    "My name is "
   end
 
   get '/hometown' do
-    resp.write "My hometown is "
+    "My hometown is "
     resp.status = 200
   end
 
